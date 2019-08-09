0a42caee5b8d9b241273c718a2a54246fb9edefd
kei <kenokohe@gmail.com>
2019-08-09 23:26:25 +0900

    mod gitignore readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # generate_caption
+
+## 実装環境
+- Windows10 Home 64bit
+- Visual Studio 2017 (C++ BuildTool)
+- Python 3.6.9 (miniconda3)
+- Chainer 5.3.0
+- Cuda 9.1
+- Cupy 6.2.0 (cupy-cuda91 6.2.0)
+
+## TODO
+- 訓練データ用意(画像とキャプション)
+- 学習
+- 新規画像の読込(部分)とキャプションの生成
--- preserve_image_caption.py
+++ preserve_image_caption.py
@@ -34,15 +34,28 @@ with open("data\yjcaptions26k_clean.json", "r", encoding="utf-8") as f:
 f = codecs.open("caption.txt", "w", "utf-8")
 o = codecs.open("img_id.txt", "w", "utf-8")
 
-for k in images:
+for image in images:
     try:
         # 画像リンクアクセス
-        res = urllib.request.urlopen(images[k])
+        res = urllib.request.urlopen(images[image])
     except urllib.error.HTTPError:
         # レスポンスエラーの場合は次処理へ
         continue
 
-    print(res.status)
+    # レスポンス読込
+    res_body = res.read()
+
+    # 画像保存
+    jpg = codecs.open(str(image) + ".jpg", "w")
+    jpg.buffer.write(res_body)
+
+    # 説明文と画像IDを保存
+    f.write(captions[image] + "\n")
+    o.write(str(image) + "\n")
 
     # クローズ処理
     res.close()
+
+# クローズ処理
+f.close()
+o.close()
