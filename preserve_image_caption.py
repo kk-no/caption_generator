46b393881455fd7f138b5b08c404e1f8ac1369d8
kei <kenokohe@gmail.com>
2019-08-11 23:15:30 +0900

    main関数作成

--- preserve_image_caption.py
+++ preserve_image_caption.py
@@ -3,60 +3,65 @@ import json
 import codecs
 import urllib.request
 
-# 画像リスト
-images = {}
-# 説明文リスト
-captions = {}
 
-with open("data\yjcaptions26k_clean.json", "r", encoding="utf-8") as f:
-    jsons = json.load(f)
-    for i in jsons["annotations"]:
-        # 説明文
-        caption = i["caption"]
-        # 画像ID
-        img_id = i["image_id"]
-        if img_id not in images:
-            # 画像のエントリを保存
-            img = [j["flickr_url"] for j in jsons["images"] if j["id"] == img_id]
-            if len(img) > 0:
-                # 画像リストの追加
-                images[img_id] = img[0]
+def main():
+    # 画像リスト
+    images = {}
+    # 説明文リスト
+    captions = {}
 
-        # 内包表記を使用しない場合
-        # if img_id not in images:
-        #     for j in jsons["images"]:
-        #         if j["id"] == img_id:
-        #             img = j["flickr_url"]
+    with open("data\yjcaptions26k_clean.json", "r", encoding="utf-8") as f:
+        jsons = json.load(f)
+        for i in jsons["annotations"]:
+            # 説明文
+            caption = i["caption"]
+            # 画像ID
+            img_id = i["image_id"]
+            if img_id not in images:
+                # 画像のエントリを保存
+                img = [j["flickr_url"] for j in jsons["images"] if j["id"] == img_id]
+                if len(img) > 0:
+                    # 画像リストの追加
+                    images[img_id] = img[0]
 
-        # 説明文リストの追加
-        captions[img_id] = caption
+            # 内包表記を使用しない場合
+            # if img_id not in images:
+            #     for j in jsons["images"]:
+            #         if j["id"] == img_id:
+            #             img = j["flickr_url"]
 
-f = codecs.open("caption.txt", "w", "utf-8")
-o = codecs.open("img_id.txt", "w", "utf-8")
+            # 説明文リストの追加
+            captions[img_id] = caption
 
-for image in images:
-    try:
-        # 画像リンクアクセス
-        res = urllib.request.urlopen(images[image])
-    except urllib.error.HTTPError:
-        # レスポンスエラーの場合は次処理へ
-        continue
+    f = codecs.open("caption.txt", "w", "utf-8")
+    o = codecs.open("img_id.txt", "w", "utf-8")
 
-    # レスポンス読込
-    res_body = res.read()
+    for image in images:
+        try:
+            # 画像リンクアクセス
+            res = urllib.request.urlopen(images[image])
+        except urllib.error.HTTPError:
+            # レスポンスエラーの場合は次処理へ
+            continue
 
-    # 画像保存
-    jpg = codecs.open(str(image) + ".jpg", "w", "utf-8")
-    jpg.buffer.write(res_body)
+        # レスポンス読込
+        res_body = res.read()
 
-    # 説明文と画像IDを保存
-    f.write(captions[image] + "\n")
-    o.write(str(image) + "\n")
+        # 画像保存
+        jpg = codecs.open(str(image) + ".jpg", "w", "utf-8")
+        jpg.buffer.write(res_body)
+
+        # 説明文と画像IDを保存
+        f.write(captions[image] + "\n")
+        o.write(str(image) + "\n")
+
+        # クローズ処理
+        jpg.close()
+        res.close()
 
     # クローズ処理
-    jpg.close()
-    res.close()
+    f.close()
+    o.close()
 
-# クローズ処理
-f.close()
-o.close()
+if __name__ == "__main__":
+    main()
